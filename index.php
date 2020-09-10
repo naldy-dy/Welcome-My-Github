0abf2fb4f154cdaaf92b4dc9bd60ede4bccbab53
sunardi <akunaku08022001@gmail.com>
2020-09-10 07:48:53 +0700

    penggunaan fungtion $this->

--- index.php
+++ index.php
@@ -2,24 +2,30 @@
 
 // penamaan kelas PHP
 	class Hp{
+
+		// nama atribut
 		public
 		$merk,
 		$tipe,
 		$display,
 		$ram,
-		$harga; 	
+		$harga;
+
+		public function cetakdisplay(){
+			return $this->display;
+		}
 	}
 
 // penginputan data kelas
 	$produk = new Hp;
 	$produk->merk = "iphone";
 	$produk->tipe = "10 pro max";
-	$produk->display = "super amoled";
+	$produk->display =ucwords("super amoled");
 	$produk->ram = "4gb";
 	$produk->harga = "16 jt";
 
 // menampilkan isi data yang dipilih di web bros
-	echo "$produk->display";
+	echo "Yang ditampilkan ".$produk->cetakdisplay();
 
 
 
