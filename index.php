6bd2cb9a5db8d34e7aa95c299a6f09899171c131
sunardi <akunaku08022001@gmail.com>
2020-09-10 08:57:47 +0700

    belajar class and method 2

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
 	}
 
 // penginputan data kelas
+
+	// produk-1
 	$produk = new Hp;
 	$produk->merk = "iphone";
 	$produk->tipe = "10 pro max";
@@ -27,10 +29,32 @@
 	$produk->ram = "4gb";
 	$produk->harga = number_format(16000000) ;
 
+	// produk-2
+	$produk2 = new Hp;
+	$produk2->merk = "iphone";
+	$produk2->tipe = "10 pro max";
+	$produk2->display =ucwords("super amoled");
+	$produk2->ram = "4gb";
+	$produk2->harga = number_format(16000000) ;
+
 // menampilkan isi data yang dipilih di web bros
-	echo "$produk->merk";
+
 	echo "Yang ditampilkan ".$produk->cetakdisplay();
+	echo "<br>";
 	echo "Harga yang ditampilkan " .$produk->tampilharga();
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	echo "$produk->merk";
+	echo "<br>";
+	echo "$produk2->merk";
+	echo "<br>";
+
+	if($produk2 === $produk){
+		echo "Produk Sama";
+	}else{
+		echo "Produk Tidak Sama";
+	}
 
 
 
